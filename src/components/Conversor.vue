b17d9a95dd2bc237524b6162d9fa4b0383e28891
fabio said <fabiosaid635@gmail.com>
2020-08-31 16:50:47 -0300

    bitcoin alert

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -18,29 +18,21 @@ export default
           moedaB_value : 0
         };
     },
-
     methods:{
         converter(){
-         
-        let de_para = this.moedaA + "_" + this.moedaB;
-
-       
+         let de_para = this.moedaA + "_" + this.moedaB;
 
          let url ="http://free.currconv.com/api/v7/convert?q=" 
          + de_para
          + "&compact=ultra&apiKey=df4c0667464e5fac1bbd";
        
-
         fetch(url)
         .then(res => {
             return res.json()
             })
         .then(json=>{
-             let cotacao = json[de_para];
-            
-            this.moedaB_value = (cotacao * parseFloat(this.moedaA_value));
-           
-        
+             let cotacao = json[de_para];     
+             this.moedaB_value = (cotacao * parseFloat(this.moedaA_value));       
         });
         }
     }
